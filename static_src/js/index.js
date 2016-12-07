c3d136bd61fcf6269a099314fb35bf16e64bb806
Sergey <s-kobets617@yandex.ru>
2016-12-07 11:52:09 +0300

    methods for localStorage

--- static_src/js/index.js
+++ static_src/js/index.js
@@ -1,5 +1,18 @@
-import Glob from './page/localStorage.js'
+import Local from './page/localStorage.js'
 
-Glob.addlocalStor();
+const stor = Local;
+// читаем что у нас в localStorage, если передать 
+// имя, то читаем соответсвующее хранилище
+let ar = stor.readStorage(); // stor.readStorage('ya-ya');
 
-console.log(Glob);
+// добавляем значения в localStorage
+stor.addlocalStor(1, 3, 4);
+
+// обнулить localStorage
+stor.nullStorage();
+
+// присвоить другое имя localStorage
+stor.nameStorage = 'ya-ya';
+stor.addlocalStor('asd', 'dfg');
+
+console.log(ar);
--- static_src/js/page/localStorage.js
+++ static_src/js/page/localStorage.js
@@ -1,27 +1,42 @@
 // Обратимся к localStorage следующим образом
-let Glob = {
-	_urlGlobal: {},
+let Local = {
+	_urlGlobal: [],
+	nameStorage: 'object',
 	// add localStorage
 	addlocalStor: function () {
+		const length = arguments.length;
+		for (let i = 0; i < length; i++) {
+			this._urlGlobal.push(arguments[i]);
+		}
+
 		// Сериализуем его
 		let sObj = JSON.stringify(this._urlGlobal);
 
 		// Запишем в localStorage с ключём object
-		localStorage.setItem('object', sObj);
+		localStorage.setItem(this.nameStorage, sObj);
 	},
 	// Получим наш сериализованный объект через API
 	// Одновременно преобразуем к обычному объекту JavaScriptJSON.parse(localStorage.getItem("object"));
-	inicial: function () {
+	readStorage: function (name) {
 		let varLocalStorage;
-
-		if ( localStorage !== undefined) {
-			varLocalStorage = JSON.parse(localStorage.getItem('object'));
+		if (name) {
+			this.nameStorage = name;
 		}
 
-		if ( varLocalStorage !== undefined) {
+		if (localStorage !== undefined) {
+			varLocalStorage = JSON.parse(localStorage.getItem(this.nameStorage));
+		}
+
+		if (varLocalStorage !== undefined) {
 			this._urlGlobal = varLocalStorage;
 		}
+		return this._urlGlobal;
+	},
+
+	nullStorage: function () {
+		this._urlGlobal = [];
+		this.addlocalStor();
 	}
 }
 
-export default Glob;
+export default Local;
