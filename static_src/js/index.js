ed24f7cd2058afffe5757bc04a8e3a06e5d3b659
Sergey <s-kobets617@yandex.ru>
2016-12-08 12:33:20 +0300

    add cookie

--- static_src/js/index.js
+++ static_src/js/index.js
@@ -4,15 +4,23 @@ const stor = new Local;
 // читаем что у нас в localStorage, если передать 
 // имя, то читаем соответсвующее хранилище
 let ar = stor.readStorage(); // stor.readStorage('ya-ya');
+let co = stor.readCookie();
 
 // добавляем значения в localStorage
 stor.addlocalStor(1, 3, 4);
+stor.addCookie();
 
 // обнулить localStorage
 stor.nullStorage();
+stor.nullCookie();
 
 // присвоить другое имя localStorage
 stor.nameStorage = 'ya-ya';
 stor.addlocalStor('asd', 'dfg');
+stor.addCookie();
 
-console.log(ar);
+// удалить cookie
+// stor.deleteCookie('object');
+
+
+console.log(stor.nameStorage);
--- static_src/js/page/localStorage.js
+++ static_src/js/page/localStorage.js
@@ -1,43 +1,113 @@
 // Обратимся к localStorage следующим образом
-let Local = function () {
-	return {
-		_urlGlobal: [],
-		nameStorage: 'object',
-		// add localStorage
-		addlocalStor: function () {
-			const length = arguments.length;
-			for (let i = 0; i < length; i++) {
-				this._urlGlobal.push(arguments[i]);
-			}
+function Local() {
+	this.urlGlobal = [];
+	this.nameStorage = 'object';
+}
 
-			// Сериализуем его
-			let sObj = JSON.stringify(this._urlGlobal);
-
-			// Запишем в localStorage с ключём object
-			localStorage.setItem(this.nameStorage, sObj);
-		},
-		// Получим наш сериализованный объект через API
-		// Одновременно преобразуем к обычному объекту JavaScriptJSON.parse(localStorage.getItem("object"));
-		readStorage: function (name) {
-			let varLocalStorage;
-			if (name) {
-				this.nameStorage = name;
-			}
-
-			if (localStorage !== undefined) {
-				varLocalStorage = JSON.parse(localStorage.getItem(this.nameStorage));
-			}
-
-			if (varLocalStorage !== undefined) {
-				this._urlGlobal = varLocalStorage;
-			}
-			return this._urlGlobal;
-		},
-
-		nullStorage: function () {
-			this._urlGlobal = [];
-			this.addlocalStor();
+Local.prototype = {
+	checkUrlGlobal: function () {
+		if (!(this.urlGlobal instanceof Array)) {
+			this.urlGlobal = [];
 		}
+	},
+
+	checkName: function (name) {
+		if (name) {
+			this.nameStorage = name;
+		} else {
+			return name = this.nameStorage;
+		}
+	},
+	// add localStorage
+	addlocalStor: function () {
+		const length = arguments.length;
+		console.log(this);
+		this.checkUrlGlobal();
+		for (let i = 0; i < length; i++) {
+			this.urlGlobal.push(arguments[i]);
+		}
+		// Сериализуем его
+		let sObj = JSON.stringify(this.urlGlobal);
+
+		// Запишем в localStorage с ключём object
+		localStorage.setItem(this.nameStorage, sObj);
+	},
+	// Получим наш сериализованный объект через API
+	// Одновременно преобразуем к обычному объекту JavaScriptJSON.parse(localStorage.getItem("object"));
+	readStorage: function (name) {
+		let varLocalStorage;
+		this.checkName(name);
+
+		if (localStorage !== undefined) {
+			varLocalStorage = JSON.parse(localStorage.getItem(this.nameStorage));
+		}
+
+		if (varLocalStorage !== undefined) {
+			this.urlGlobal = varLocalStorage;
+		}
+		return this.urlGlobal;
+	},
+
+	nullStorage: function () {
+		this.urlGlobal = [];
+		this.addlocalStor();
+	},
+
+	optionCookie: function (options) {
+		options = options || {};
+		let expires = options.expires;
+
+		if (typeof expires == "number" && expires) {
+			let d = new Date();
+			d.setTime(d.getTime() + expires * 1000);
+			expires = options.expires = d;
+		}
+		if (expires && expires.toUTCString) {
+			options.expires = expires.toUTCString();
+			return options;
+		}
+	},
+	// add localStorage
+	addCookie: function (name, options) {
+		const optCookie = this.optionCookie(options);
+		this.checkName(name);
+
+		let updatedCookie = `${this.nameStorage}=${this.urlGlobal.join(',')}`;		
+		if (optCookie) {
+			for (let propName in optCookie) {
+		    updatedCookie += "; " + propName;
+		    let propValue = optCookie[propName];
+		    if (propValue !== true) {
+		      updatedCookie += "=" + propValue;
+		    }
+		  }
+		}
+
+	  document.cookie = updatedCookie;
+	},
+	// возвращает cookie с именем name, если есть, если нет, то undefined
+	readCookie: function (name) {
+		if (navigator.cookieEnabled) {
+			const nameCookie = this.checkName(name);
+			const matches = document.cookie.match(new RegExp(
+				"(?:^|; )" + nameCookie.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+			));
+
+			return matches ? decodeURIComponent(matches[1]) : undefined;
+		} else {
+			alert( 'Включите cookie для комфортной работы с этим сайтом' );
+		}
+	},
+
+	nullCookie: function () {
+		this.urlGlobal = [];
+		this.addCookie();
+	},
+
+	deleteCookie: function (name) {
+		this.addCookie(name, {
+			expires: -1
+		})
 	}
 }
 
