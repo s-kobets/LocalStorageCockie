2cf5a4639a001b41c5c27ac9f24111ef161fe43b
Sergey <s-kobets617@yandex.ru>
2016-12-08 17:40:22 +0300

    no local param

--- static_src/js/index.js
+++ static_src/js/index.js
@@ -1,26 +1,28 @@
 import Local from './page/localStorage.js'
 
 const stor = new Local;
-// читаем что у нас в localStorage, если передать 
-// имя, то читаем соответсвующее хранилище
-let ar = stor.readStorage(); // stor.readStorage('ya-ya');
-let co = stor.readCookie();
+
+// присвоить другое имя localStorage
+// на подобии курсора для плагина
+stor.checkName('ya-ya');
 
 // добавляем значения в localStorage
 stor.addlocalStor(1, 3, 4);
-stor.addCookie();
+stor.addCookie(1, 3, 4);
 
 // обнулить localStorage
 stor.nullStorage();
 stor.nullCookie();
 
-// присвоить другое имя localStorage
-stor.nameStorage = 'ya-ya';
-stor.addlocalStor('asd', 'dfg');
-stor.addCookie();
+// читаем что у нас в localStorage, если передать 
+// имя, то читаем соответсвующее хранилище
+let ar = stor.readStorage('ya-ya'); // stor.readStorage('ya-ya');
+let co = stor.readCookie('ya-ya');
+
+console.log(ar, stor.nameStorage, stor.urlGlobal);
+// добавляем значения в localStorage
+stor.addlocalStor(5, 6, 7);
+stor.addCookie(5, 6, 7);
 
 // удалить cookie
 // stor.deleteCookie('object');
-
-
-console.log(stor.nameStorage);
--- static_src/js/page/localStorage.js
+++ static_src/js/page/localStorage.js
@@ -1,114 +1,113 @@
 // Обратимся к localStorage следующим образом
-function Local() {
-	this.urlGlobal = [];
-	this.nameStorage = 'object';
-}
+function Local() {};
 
-Local.prototype = {
-	checkUrlGlobal: function () {
-		if (!(this.urlGlobal instanceof Array)) {
-			this.urlGlobal = [];
-		}
-	},
+Local.prototype.checkUrlGlobal = function () {
+	if (!(this.urlGlobal instanceof Array)) {
+		this.urlGlobal = [];
+	}
+};
 
-	checkName: function (name) {
-		if (name) {
-			this.nameStorage = name;
-		} else {
-			return name = this.nameStorage;
-		}
-	},
-	// add localStorage
-	addlocalStor: function () {
-		const length = arguments.length;
-		console.log(this);
-		this.checkUrlGlobal();
-		for (let i = 0; i < length; i++) {
-			this.urlGlobal.push(arguments[i]);
-		}
-		// Сериализуем его
-		let sObj = JSON.stringify(this.urlGlobal);
+Local.prototype.checkName = function (name) {
+	if (name) {
+		return this.nameStorage = name;
+	} else {
+		return this.nameStorage = 'default';
+	}
+};
 
-		// Запишем в localStorage с ключём object
-		localStorage.setItem(this.nameStorage, sObj);
-	},
+// add localStorage
+Local.prototype.addlocalStor = function () {
+	const length = arguments.length;
+	this.checkUrlGlobal();
+	// this.checkName();
+	for (let i = 0; i < length; i++) {
+		this.urlGlobal.push(arguments[i]);
+	}
+	// Сериализуем его
+	let sObj = JSON.stringify(this.urlGlobal);
+
+	// Запишем в localStorage с ключём object
+	localStorage.setItem(this.nameStorage, sObj);
+};
+
 	// Получим наш сериализованный объект через API
 	// Одновременно преобразуем к обычному объекту JavaScriptJSON.parse(localStorage.getItem("object"));
-	readStorage: function (name) {
-		let varLocalStorage;
-		this.checkName(name);
+Local.prototype.readStorage = function (name) {
+	let varLocalStorage;
+	this.checkName(name);
 
-		if (localStorage !== undefined) {
-			varLocalStorage = JSON.parse(localStorage.getItem(this.nameStorage));
-		}
-
-		if (varLocalStorage !== undefined) {
-			this.urlGlobal = varLocalStorage;
-		}
-		return this.urlGlobal;
-	},
-
-	nullStorage: function () {
-		this.urlGlobal = [];
-		this.addlocalStor();
-	},
-
-	optionCookie: function (options) {
-		options = options || {};
-		let expires = options.expires;
-
-		if (typeof expires == "number" && expires) {
-			let d = new Date();
-			d.setTime(d.getTime() + expires * 1000);
-			expires = options.expires = d;
-		}
-		if (expires && expires.toUTCString) {
-			options.expires = expires.toUTCString();
-			return options;
-		}
-	},
-	// add localStorage
-	addCookie: function (name, options) {
-		const optCookie = this.optionCookie(options);
-		this.checkName(name);
-
-		let updatedCookie = `${this.nameStorage}=${this.urlGlobal.join(',')}`;		
-		if (optCookie) {
-			for (let propName in optCookie) {
-		    updatedCookie += "; " + propName;
-		    let propValue = optCookie[propName];
-		    if (propValue !== true) {
-		      updatedCookie += "=" + propValue;
-		    }
-		  }
-		}
-
-	  document.cookie = updatedCookie;
-	},
-	// возвращает cookie с именем name, если есть, если нет, то undefined
-	readCookie: function (name) {
-		if (navigator.cookieEnabled) {
-			const nameCookie = this.checkName(name);
-			const matches = document.cookie.match(new RegExp(
-				"(?:^|; )" + nameCookie.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-			));
-
-			return matches ? decodeURIComponent(matches[1]) : undefined;
-		} else {
-			alert( 'Включите cookie для комфортной работы с этим сайтом' );
-		}
-	},
-
-	nullCookie: function () {
-		this.urlGlobal = [];
-		this.addCookie();
-	},
-
-	deleteCookie: function (name) {
-		this.addCookie(name, {
-			expires: -1
-		})
+	if (localStorage !== undefined) {
+		varLocalStorage = JSON.parse(localStorage.getItem(this.nameStorage));
 	}
+
+	if (varLocalStorage !== undefined) {
+		this.urlGlobal = varLocalStorage;
+	}
+	return this.urlGlobal;
+},
+
+Local.prototype.nullStorage = function () {
+	this.urlGlobal = [];
+	this.addlocalStor();
+},
+
+Local.prototype.optionCookie = function (options) {
+	options = options || {};
+	let expires = options.expires;
+
+	if (typeof expires == "number" && expires) {
+		let d = new Date();
+		d.setTime(d.getTime() + expires * 1000);
+		expires = options.expires = d;
+	}
+	if (expires && expires.toUTCString) {
+		options.expires = expires.toUTCString();
+		return options;
+	}
+};
+
+// add localStorage
+Local.prototype.addCookie = function (name, options) {
+	const optCookie = this.optionCookie(options);
+
+	let updatedCookie = `${this.nameStorage}=${this.urlGlobal.join(',')}`;		
+	if (optCookie) {
+		for (let propName in optCookie) {
+	    updatedCookie += "; " + propName;
+	    let propValue = optCookie[propName];
+	    if (propValue !== true) {
+	      updatedCookie += "=" + propValue;
+	    }
+	  }
+	}
+
+  document.cookie = updatedCookie;
+};
+
+	// возвращает cookie с именем name, если есть, если нет, то undefined
+Local.prototype.readCookie = function (name) {
+	if (navigator.cookieEnabled) {
+		const nameCookie = this.checkName(name);
+		const matches = document.cookie.match(new RegExp(
+			"(?:^|; )" + nameCookie.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		));
+
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	} else {
+		alert( 'Включите cookie для комфортной работы с этим сайтом' );
+	}
+};
+
+Local.prototype.nullCookie = function () {
+	this.urlGlobal = [];
+	this.addCookie();
+};
+
+Local.prototype.deleteCookie = function (name) {
+	this.addCookie(name, {
+		expires: -1
+	})
 }
+
 
 export default Local;
